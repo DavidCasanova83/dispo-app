5f6ef5c145175160396303bb392686b6d51e4691
David Casanova <casanova.83130@gmail.com>
2025-06-25 09:48:40 +0200

    git init page test dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    // Nouvelle route pour la page de test
+    Route::view('test', 'test')->name('test');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
