182f29bacd1990a8d6ba4f07c8207df19743011b
Panel Admin verdontourisme <panelverdon@vmverdon25.r3zo.net>
2025-11-07 20:17:20 +0100

    init prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // middleware d'approbation
+        $middleware->alias([
+            'approved' => \App\Http\Middleware\EnsureUserIsApproved::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
